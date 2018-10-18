fcfd30e67b6595dbacb09f6404ffc5c352a9ee2f
nishikawa <nishikawa@nxpg.net>
2018-10-18 14:31:24 +0900

    no message

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -10,11 +10,39 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function(event) {});
 
 
-self.addEventListener('push', function(event) {
+// self.addEventListener('push', function(event) {
+//     event.waitUntil(
+//         self.registration.showNotification(event.data.json().notification.title, {
+//             'body': event.data.json().notification.body,
+//             'icon': './icon.png'
+//         })
+//     );
+// });
+
+
+self.addEventListener("push", function(event) {
     event.waitUntil(
-        self.registration.showNotification(event.data.json().notification.title, {
-            'body': event.data.json().notification.body,
-            'icon': './icon.png'
+      self.registration.pushManager.getSubscription()
+        .then(function(subscription) {
+          if (subscription) {
+            return subscription.endpoint
+          }
+          throw new Error('User not subscribed')
+      })
+      .then(function(res) {
+        return fetch('/notifications.json')
+      })
+      .then(function(res) {
+        if (res.status === 200) {
+          return res.json()
+        }
+        throw new Error('notification api response error')
+      })
+      .then(function(res) {
+        return self.registration.showNotification(res.title, {
+          icon: '/icon.png',
+          body: res.body
         })
-    );
-});
+      })
+    )
+  })
