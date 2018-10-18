d4c5b7a6c035a5ba48aa721e0429e59ff60bd49d
nishikawa <nishikawa@nxpg.net>
2018-10-18 13:16:10 +0900

    no message

--- src/js/import/common.js
+++ src/js/import/common.js
@@ -30,6 +30,7 @@ class initSet {
 
 		if ('serviceWorker' in navigator) {
 			// サービスワーカーの登録
+			console.log("aaa");
 			navigator.serviceWorker.register('../service-worker.js').then(function(registration) {
 				// サービスワーカー登録成功
 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
