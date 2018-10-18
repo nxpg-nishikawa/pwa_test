0543c2e23a0c0e73e693efb35c0a921acffb77cb
nishikawa <nishikawa@nxpg.net>
2018-10-18 12:49:39 +0900

    push 追加

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,3 +8,13 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+
+self.addEventListener('push', function(event) {
+    event.waitUntil(
+        self.registration.showNotification(event.data.json().notification.title, {
+            'body': event.data.json().notification.body,
+            'icon': '/icon.png'
+        })
+    );
+});
--- src/js/import/common.js
+++ src/js/import/common.js
@@ -25,6 +25,44 @@ function inviewFunc(op) {
 
 class initSet {
 	DOMReadBefore(op) {
+
+		const messaging = firebase.messaging();
+
+		if ('serviceWorker' in navigator) {
+			// サービスワーカーの登録
+			navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+				// サービスワーカー登録成功
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+				// 今回はこのサービスワーカーを指定します
+				messaging.useServiceWorker(registration);
+				// 通知の受信許可を確認する
+				messaging.requestPermission()
+						.then(function() {
+							console.log('Notification permission granted.');
+							// トークンを取得する
+							messaging.getToken()
+									.then(function(currentToken) {
+										if (currentToken) {
+											// 今回はコンソールに出力して取得します。実運用では適切な方法で保管する必要があります。
+											console.log(currentToken);
+										} else {
+											console.log('No Instance ID token available. Request permission to generate one.');
+										}
+									})
+									.catch(function(err) {
+										console.log('An error occurred while retrieving token. ', err);
+									});
+						})
+						.catch(function(err) {
+							console.log('Unable to get permission to notify.', err);
+						});
+			}).catch(function(err) {
+				// サービスワーカー登録失敗
+				console.log('ServiceWorker registration failed: ', err);
+			});
+		}
+
+
 		smoothScrollSet(op.SMOOTH);
 	}
 	DOMReadAfter(op) {
